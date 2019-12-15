ea175c23dafd9ba26d94b771f8940f248a81d13b
twyaya <51527338+twyaya@users.noreply.github.com>
2019-12-15 21:18:36 +0800

    建立

---
+++ index.js
@@ -0,0 +1,18 @@
+new Vue({
+	el: '#app',
+	data:{
+		Home: '首頁',
+		Product: '各項車款',
+		Account: '帳號',
+		Sign_in: '登入',
+		Sign_up: '註冊',
+		Call_a_car: '叫車',
+		users: [],
+		cars: ["Tesla", "BMW","Fiat Chrysler", "Ford", "GM",],
+	}
+
+})
+
+/*
+當屬標滑到選上 active 激活
+*/
